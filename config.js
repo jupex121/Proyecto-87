ada917a4cfcb7313d0755553a62770932f011b5a
jupex121 <89226846+jupex121@users.noreply.github.com>
2022-06-22 16:58:02 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXIpyver2nFTgXJ6Q23k5yncoQJDsnnHY",
+  authDomain: "narrador-de-historia.firebaseapp.com",
+  databaseURL: "https://narrador-de-historia-default-rtdb.firebaseio.com",
+  projectId: "narrador-de-historia",
+  storageBucket: "narrador-de-historia.appspot.com",
+  messagingSenderId: "920784708651",
+  appId: "1:920784708651:web:9e6785f022be526e69b66e"
+};
